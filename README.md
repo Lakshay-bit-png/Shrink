5d182bc9ce9e098e65d21fbbe4880b1c5785f538
Lakshay-bit-png <lakshaygargd@gmail.com>
2024-06-27 20:32:41 +0530

    v2

---
+++ README.md
@@ -0,0 +1,57 @@
+# Rapid Shrink Package
+
+This package provides functionality to generate redirect URLs. It is designed to be simple and easy to integrate into any Node.js application.
+
+## Installation
+
+You can install the package using npm:
+
+```sh
+npm install rapid-shrink
+```
+
+## Usage 
+
+### Generating a Shortened URL in Node.js
+To generate a shortened URL, you can use the generate function provided by the package:
+
+```sh
+const { generate } = require('rapid-shrink');
+
+(async () => {
+  try {
+    const link = 'https://example.com';
+    const redirectUrl = await generate(link);
+    console.log('Redirect URL:', redirectUrl);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
+```
+
+
+### Generating a Shortened URL in Bash
+You can use Node.js to run a JavaScript file from the command line:
+
+Create a JavaScript file , for example : shorten.js:
+
+```sh
+const { generate } = require('rapid-shrink');
+
+(async () => {
+  try {
+    const link = 'https://example.com';
+    const redirectUrl = await generate(link);
+    console.log('Redirect URL:', redirectUrl);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+})();
+
+```
+
+Run the script using Node.js:
+
+```sh
+node shorten.js
+```
--- index.js
+++ index.js
@@ -1,67 +1,49 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const route = require('./routes/route');
 
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Middleware to parse JSON requests
-app.use(express.json());
-
-connectDB();
-// Use API routes
-app.use('/',route);
-// index.js
-
-
-async function callBackendRoute(link) {
-    let url = 'http://localhost:3000'; 
-    
-    try {
-     
-      url = `${url}/save`;
+async function generate(link) {
+  let url = 'https://seal-app-qq2pf.ondigitalocean.app'; 
   
-    
-      const x = await postData(url, { to: link });
-  
-     
-      if (!x.data.to.startsWith('http://') && !x.data.to.startsWith('https://')) {
-        x.data.to = `https://${x.data.to}`;
-      }
-  
-      return `http://localhost:3000/${x.data.from}` ; 
+  try {
+   
+    url = `${url}/api/website/handler`;
 
   
-    } catch (error) {
-      console.error('Error calling backend route:', error);
+    const x = await postData(url, { to: link });
+
+   
+    if (!x.data.to.startsWith('http://') && !x.data.to.startsWith('https://')) {
+      x.data.to = `https://${x.data.to}`;
     }
-  }
-  
-  async function postData(url, data) {
-    const fetch = await import('node-fetch');
-    const response = await fetch.default(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    return response.json();
-  }
-  
-  async function fetchData(url) {
-    const fetch = await import('node-fetch');
-    const response = await fetch.default(url);
-    return response.json();
-  }
-  
-  module.exports = {
-    callBackendRoute,
-  };
-  
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+    return `https://seal-app-qq2pf.ondigitalocean.app/r?r=${x.data.from}` ; 
+
+
+  } catch (error) {
+    console.error('Error calling backend route:', error);
+  }
+}
+
+async function postData(url, data) {
+  const fetch = await import('node-fetch');
+  const response = await fetch.default(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+
+async function fetchData(url) {
+  const fetch = await import('node-fetch');
+  const response = await fetch.default(url);
+  return response.json();
+}
+
+module.exports = {
+  generate,
+};
+
+
+
